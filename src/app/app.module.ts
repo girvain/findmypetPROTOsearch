cf773b22f52c0cff159fa5e671f8c745fd4dc4f8
girvain <gavinross88@hotmail.co.uk>
2020-02-26 02:41:02 +0000

    safd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { CatCardComponent } from './cat-card/cat-card.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatCardComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatButtonModule, 
+    FlexLayoutModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
